f7fe32d5fe639aa633fe003d3e6cbbc04699e2ac
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-11 18:23:20 +0200

    lavender: Drop product_launched_with_*.mk
    * It seems Google stopped making those
    * Directly set PRODUCT_SHIPPING_API_LEVEL
    
    Change-Id: I2e31da85e68a8f3ef4ce51bd4decd40dfe075a0d

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Inherit from sdm660-common
 $(call inherit-product, device/xiaomi/sdm660-common/sdm660.mk)
 
+# PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
+PRODUCT_SHIPPING_API_LEVEL := 28
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
